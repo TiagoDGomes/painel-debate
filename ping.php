3cf85588da5800376de653b3b17950e1f9217c8b
Tiago Gomes <tiagodgomes@ymail.com>
2020-09-29 08:52:37 -0300

    ping

--- ping.php
+++ ping.php
@@ -1,17 +1,18 @@
 <?php
 
-if (isset($_GET['t'])){
+if (isset($_GET['t'])) {
     header('Content-Type: application/json');
-    $dados = array();    
-    $microtime_user = (int) @$_GET['t'];    
+    $dados = array();
+    $microtime_user = (int) @$_GET['t'];
     $dados['t'] = (int) (microtime(1) * 1000);
-    $dados['diff'] =  $dados['t'] - $microtime_user;    
+    $dados['diff'] =  $dados['t'] - $microtime_user;
     echo json_encode($dados);
-    exit();    
+    exit();
 }
 
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
 
 <head>
@@ -25,7 +26,7 @@ if (isset($_GET['t'])){
     <div id="result"></div>
 
     <script>
-        var timestamp_server = 0;
+        var real_timestamp_server = 0;
         var last_diff = 0;
 
         function ping(timestamp) {
@@ -35,18 +36,20 @@ if (isset($_GET['t'])){
                     var dados = JSON.parse(xhr.responseText);
                     var result = document.getElementById('result');
                     result.innerHTML = dados['diff'] + 'ms';
-                    timestamp_server = dados['t'];
+                    real_timestamp_server = dados['t'];
                     last_diff = dados['diff'];
                     if (timestamp == 1000) {
-                        ping(timestamp_server);
+                        ping(real_timestamp_server);
                     }
+                    tempo_referencia = Math.round(real_timestamp_server / 1000);
                 }
             };
             xhr.open('GET', 'ping.php?t=' + timestamp);
             xhr.send();
         }
+
         setInterval(function() {
-            ping(timestamp_server + 1000 - last_diff, true);
+            ping(timestamp_server + 1000, true);
         }, 1000);
     </script>
 </body>
