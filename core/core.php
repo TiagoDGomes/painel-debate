4b75db477d0f96e064597063c3470b395dbe6b15
Tiago <tiagodgomes@ymail.com>
2024-10-10 14:14:40 -0300

    Valores padrão

--- core/core.php
+++ core/core.php
@@ -7,7 +7,20 @@ define('PCORE', true);
 
 session_start();
 
-require_once 'config.php';
+@include_once 'config.php';
+
+if (!isset($DATABASE_PATH)){
+    exit('$DATABASE_PATH is not definied.');
+}
+if (!isset($MAX_DATABASES)){
+    $MAX_DATABASES = 6;
+}
+if (!isset($MAX_DATABASES)){
+    $ID_LENGTH = 12;
+}
+if (!isset($MAX_DATABASES)){
+    $SYNC_PING_COUNT = 4;
+}
 require_once 'classes.php';
 
 
